b71bbd419f0ad5b1b253c33d68e418867be76a7e
tmccann <134166450+tmccann@users.noreply.github.com>
2024-11-20 15:31:46 +0000

    Update FormContainer.jsx
    change class name to match component

--- src/forms/FormContainer/FormContainer.jsx
+++ src/forms/FormContainer/FormContainer.jsx
@@ -2,7 +2,7 @@ import styles from "./formContainer.module.css";
 
 const FormContainer = ({ title, children }) => {
   return (
-    <div className={styles.formWrapper}>
+    <div className={styles.form_container }>
       <div className={styles.form}>
         <h2>Add New {title}</h2>
         {children}
